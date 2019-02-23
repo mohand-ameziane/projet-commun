ca034985373449553633865e6681032ecb27e906
Stili Mehdi <mehdidi93420@gmail.com>
2019-02-23 03:48:26 +0100

    design page information

--- src/pages/information/information.ts
+++ src/pages/information/information.ts
@@ -14,6 +14,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'information.html',
 })
 export class InformationPage {
+	
+	public buttonColor : string[] = ['lightblue','lightblue','lightblue','lightblue','lightblue'];
+	public homme : string = 'lightblue';
+	public femme : string = 'lightblue';
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
@@ -24,5 +28,52 @@ export class InformationPage {
   allepageinformation(){
     this.navCtrl.push('HomePage');
   }
+  
+  addEventL1(){
+	this.buttonColor[0] = 'blue';
+	this.negateOtherColor(0);
+  }
+  
+  addEventL2(){
+	this.buttonColor[1] = 'blue';
+	this.negateOtherColor(1);
+  }                  
+  
+  addEventL3(){
+	this.buttonColor[2] = 'blue';
+	this.negateOtherColor(2);
+  }
+  
+  addEventM1(){
+	this.buttonColor[3] = 'blue';
+	this.negateOtherColor(3);
+  }
+  
+  addEventM2(){
+	this.buttonColor[4] = 'blue';
+	this.negateOtherColor(4);
+  }
+  
+  negateOtherColor(i):void{
+	  for(var k=0;k<5;k++){
+		if(k!=i){
+			this.buttonColor[k]='lightblue';
+		}
+	  }
+  }
+  
+  
+  addEventHomme(){
+	this.homme='blue';
+	this.femme='lightblue';
+  }
+  
+  addEventFemme(){
+	this.femme='blue';
+	this.homme='lightblue';
+	  
+  }
+  
+ 
 
 }
